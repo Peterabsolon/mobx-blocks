00cb0cfda061d47879f33da8f18de05d31c5c205
Peter Absolon <peterabsolon@yahoo.com>
2022-12-06 15:02:56 +0100

    chore: use official @types/debounce-promise

--- src/lib/@types/debounce-promise.d.ts
+++
@@ -1,9 +0,0 @@
-type TFn = (...args: any) => any
-
-declare module "debounce-promise" {
-  type TDebounceFn = (fn: TFn, ms: number) => TFn
-
-  const debounce: TDebounceFn
-
-  export default debounce
-}
