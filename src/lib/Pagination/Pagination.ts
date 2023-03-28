facb80c35fe628389e245d90ebae8241f7962178
Peter Absolon <peterabsolon@yahoo.com>
2023-03-28 10:06:54 +0200

    feat: [Pagination] goToPage()

--- src/lib/Pagination/Pagination.ts
+++ src/lib/Pagination/Pagination.ts
@@ -18,7 +18,7 @@ export class Pagination {
   // ====================================================
   page = 1
   pageSize = 20
-  totalCount?: number
+  totalCount?: number = undefined
 
   // ====================================================
   // Constructor
@@ -35,11 +35,21 @@ export class Pagination {
   // Computed
   // ====================================================
   get canGoToNext() {
-    if (!this.totalCount) {
+    if (this.totalCount === undefined) {
       return true
     }
 
     return this.page * this.pageSize < this.totalCount
+  }
+
+  canGoToPage = (page: number) => {
+    if (this.totalCount === undefined) {
+      return true
+    }
+
+    const count = page * this.pageSize
+
+    return this.page > 0 && count <= this.totalCount
   }
 
   get params(): IPaginationParams {
@@ -64,33 +74,57 @@ export class Pagination {
     this.pageSize = size
   }
 
+  onChange = () => {
+    if (this.config?.onChange) {
+      this.config.onChange(this.params)
+    }
+  }
+
+  /**
+   * Set the page, trigger onChange
+   */
+  goToPage = (page: number) => {
+    if (this.canGoToPage(page)) {
+      this.setPage(page)
+      this.onChange()
+    }
+  }
+
+  /**
+   * Go to previous page, trigger onChange
+   */
   goToPrev = () => {
     if (this.page > 1) {
       this.page -= 1
+      this.onChange()
     }
   }
 
+  /**
+   * Go to next page, trigger onChange
+   */
   goToNext = () => {
-    const { onChange } = this.config || {}
-
     if (this.canGoToNext) {
       this.page += 1
-
-      if (onChange) {
-        onChange(this.params)
-      }
+      this.onChange()
     }
   }
 
+  /**
+   * Reset state to initial passed through constructor
+   */
   resetToInitial = () => {
     this.page = this.config?.page ?? 1
     this.pageSize = this.config?.pageSize ?? 20
-    this.totalCount = this.config?.totalCount ?? 0
+    this.totalCount = this.config?.totalCount ?? undefined
   }
 
+  /**
+   * Completely reset state
+   */
   reset = () => {
     this.page = 1
     this.pageSize = 20
-    this.totalCount = 0
+    this.totalCount = undefined
   }
 }
